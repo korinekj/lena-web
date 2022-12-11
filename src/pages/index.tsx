d8d844e3fb3352b251fee90595152a3ecd02b3ab
Jarda Korinek <korinek14.j@gmail.com>
2022-12-11 19:44:21 +0100

    add eslint + prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 
 export default function Home() {
-  const test: number = 5;
+  const test: string = 5;
+
+  console.log(test);
+
   return <div>Hello world!</div>;
 }
