e7b747401353f9de4e6b816010bf247a18bbfff2
Jarda Korinek <korinek14.j@gmail.com>
2022-12-13 23:20:32 +0100

    just testing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "gatsby";
 
 function Navbar() {
-  return <nav>navbar</nav>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">home link</Link>
+          <button type="button">hamburger</button>{" "}
+        </div>
+        <div>nav-links</div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
