cf3296321d619f8a1f4880c668b79e7a25bca681
Jarda Korinek <korinek14.j@gmail.com>
2023-01-07 12:38:49 +0100

    add footer
    -added basic structure and content to footer
    -added basic style with flexbox
    -added mui icon
    -added image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,50 @@
 import React from "react";
 
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import ChefIcon from "../assets/images/chef-avatar.png";
+
+import * as styles from "./footer.module.scss";
+
 function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='main-footer'>
+      <div className={styles.container}>
+        <div className={styles.about}>
+          <h4>Kdo jsem</h4>
+          <img src={ChefIcon} alt='' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Repudiandae obcaecati, modi debitis dignissimos provident excepturi!
+          </p>
+        </div>
+
+        <div className={styles.logo}>FOOTER LOGO / ODKAZ NA HOME</div>
+
+        <div className={styles.contact}>
+          <h4>Kontaktní informace</h4>
+          <ul>
+            <li>
+              <FavoriteBorderIcon />
+              <p>Název cukrářství</p>
+            </li>
+            <li>
+              <PhoneIphoneIcon />
+              <p>Telefon číslo</p>
+            </li>
+            <li>
+              <EmailOutlinedIcon />
+              <p>Email</p>
+            </li>
+            <li>
+              <LocationOnOutlinedIcon />
+              <p>Adresa</p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
