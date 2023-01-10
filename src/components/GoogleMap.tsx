0f9dc5e0f7e1a85657753f4e43c07703ef4175f7
Jarda Korinek <korinek14.j@gmail.com>
2023-01-10 22:19:55 +0100

    refactor google map as a component
    -created google map component
    -rendered bellow footer on index page

---
+++ src/components/GoogleMap.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import * as styles from "./googleMap.module.scss";
+
+function GoogleMap() {
+  return (
+    <div className={styles.mapContainer}>
+      <iframe
+        className={styles.map}
+        title='map'
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2604.1213565533553!2d15.19457071585113!3d49.25514568064204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470cd94dfc8f6eab%3A0x4ce636c7d6eaeb9b!2zTmEgVsO9c2x1bsOtIDYzMiwgMzk0IDY4IMW9aXJvdm5pY2U!5e0!3m2!1scs!2scz!4v1673302735256!5m2!1scs!2scz'
+        width='600'
+        height='350'
+        style={{ border: 0 }}
+        allowFullScreen
+        loading='lazy'
+        referrerPolicy='no-referrer-when-downgrade'
+      />
+    </div>
+  );
+}
+
+export default GoogleMap;
