48c5a3e88f44cd4171d96f86597912fb5faa0a14
Jarda Korinek <korinek14.j@gmail.com>
2023-01-11 22:34:52 +0100

    add css reset and create kontakt page
    -added basic css reset box-sizing: border box; margin 0; padding 0
    -created new page - kontakt

---
+++ src/pages/kontakt.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GoogleMap from "../components/GoogleMap";
+import Layout from "../components/Layout";
+
+export default function Kontakt() {
+  return (
+    <Layout>
+      <section className='contact'>
+        <h1>kontakt</h1>
+      </section>
+      <GoogleMap />
+    </Layout>
+  );
+}
