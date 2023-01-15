4c1d3d3a350a4e5ac25ea4aecec8fe5fd3f47cbd
Jarda Korinek <korinek14.j@gmail.com>
2023-01-15 20:04:05 +0100

    complete hamburger menu
    -on small devices there will be hamburger menu that user can click to show all header navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,18 @@ import { FiAlignJustify } from "react-icons/fi";
 import * as styles from "./header.module.scss";
 
 function Header() {
+  const toggleLinks = () => {
+    const links = document.querySelectorAll(`.${styles.header__navLinks}`)[0];
+    links.classList.toggle(styles.showLinks);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.navHeader}>
         <div className='header__logo'>
           <h1>LOGO</h1>
         </div>
-        <button type='button' className={styles.navBtn}>
+        <button type='button' className={styles.navBtn} onClick={toggleLinks}>
           <FiAlignJustify />
           test
         </button>
