67ca065bdccce9c184ff1fcf477ddc3211a54394
Jarda Korinek <korinek14.j@gmail.com>
2023-03-24 00:58:48 +0100

    Working on lightbox

--- src/components/DortyGallery.tsx
+++ src/components/DortyGallery.tsx
@@ -12,12 +12,14 @@ function DortyGallery() {
 
   return (
     <section className={styles.imgWrapper}>
-      {data.allFile.nodes.map((item, index) => (
-        <article key={index} className={styles.img}>
-          <GatsbyImage
-            image={item.childImageSharp.gatsbyImageData}
-            alt='test'
-          />
+      {data.allFile.nodes.map((item) => (
+        <article key={item.id} className={styles.img}>
+          <a href={item.publicURL}>
+            <GatsbyImage
+              image={item.childImageSharp.gatsbyImageData}
+              alt={item.base}
+            />
+          </a>
         </article>
       ))}
     </section>
@@ -28,6 +30,9 @@ const query = graphql`
   {
     allFile(filter: { sourceInstanceName: { eq: "dorty" } }) {
       nodes {
+        id
+        base
+        publicURL
         childImageSharp {
           gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, width: 250)
         }
