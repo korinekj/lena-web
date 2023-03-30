c9b6abc57eae60cc37b9de1afa07301c7634da52
Jarda Korinek <korinek14.j@gmail.com>
2023-03-30 23:56:00 +0200

    edit duration of the animation used to resize the lightbox when the image changes

--- src/assets/js/lightbox-plus-jquery.js
+++ src/assets/js/lightbox-plus-jquery.js
@@ -11337,7 +11337,7 @@
     // maxWidth: 800,
     // maxHeight: 600,
     positionFromTop: 50,
-    resizeDuration: 700,
+    resizeDuration: 300,
     showImageNumberLabel: true,
     wrapAround: false,
     disableScrolling: false,
