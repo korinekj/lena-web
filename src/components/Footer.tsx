78c93acd5bdd2980cc5bfb0b555ee09ac82d8ed9
Jarda Korinek <korinek14.j@gmail.com>
2023-05-23 23:29:07 +0200

    Changed approach
    -using object destructuring for classes
    -using CSS modules

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,51 +9,52 @@ import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ChefIcon from "../assets/images/chef-avatar.png";
 import Logo from "../assets/images/logo.png";
 
+import * as GLOBAL from "../assets/css/global.module.scss";
 import * as styles from "./footer.module.scss";
 
 function Footer() {
+  const { site__footer, col, chef, logo, nav, nav__list, nav__item } = styles;
+
   return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
-        <div className={`${styles.about} ${styles.containerColumn}`}>
+    <footer className={site__footer}>
+      <div className={GLOBAL.container}>
+        <div className={col}>
           <h4>Kdo jsem</h4>
-          <img src={ChefIcon} alt='' className={styles.chef} />
-          <p>
-            Unikátní a lahodné dorty na každou příležitost! Kontaktujte mě pro
-            nejlepší dort na Vaší speciální událost.
-          </p>
+          <img src={ChefIcon} alt='' className={chef} />
           <p>
             Vaše speciální událost si zaslouží speciální dort. Dejte mi vědět a
             já Vám připravím unikátní a lahodný dort na míru
           </p>
         </div>
 
-        <div className={`${styles.logo} ${styles.containerColumn}`}>
+        <div className={col}>
           <Link to='/'>
-            <img src={Logo} alt='logo' id={styles.logo} />
+            <img src={Logo} alt='logo' id={logo} />
           </Link>
         </div>
 
-        <div className={`${styles.contact} ${styles.containerColumn}`}>
+        <div className={col}>
           <h4>Kontaktní informace</h4>
-          <ul>
-            <li>
-              <FavoriteBorderIcon />
-              <p>Název cukrářství</p>
-            </li>
-            <li>
-              <PhoneIphoneIcon />
-              <p>Telefon číslo</p>
-            </li>
-            <li>
-              <EmailOutlinedIcon />
-              <p>Email</p>
-            </li>
-            <li>
-              <LocationOnOutlinedIcon />
-              <p>Adresa</p>
-            </li>
-          </ul>
+          <nav className={nav}>
+            <ul className={nav__list}>
+              <li className={nav__item}>
+                <FavoriteBorderIcon />
+                <p>Název cukrářství</p>
+              </li>
+              <li className={nav__item}>
+                <PhoneIphoneIcon />
+                <p>Telefon číslo</p>
+              </li>
+              <li className={nav__item}>
+                <EmailOutlinedIcon />
+                <p>Email</p>
+              </li>
+              <li className={nav__item}>
+                <LocationOnOutlinedIcon />
+                <p>Adresa</p>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </footer>
--- src/pages/o-mne.tsx
+++ src/pages/o-mne.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-import * as global from "../assets/css/index.module.scss";
+import * as global from "../assets/css/global.module.scss";
 
 function About() {
+  const { container } = global;
+
   return (
     <Layout>
-      <main className={global.page}>
-        <h1>Stránka o mě</h1>
-        <section>
-          <p>
-            Ahoj, mé jméno je Lenka a pečení dortů je mým koníčkem. Mým cílem je
-            poskytnout zákazníkům dorty, které nejen vynikají chutí, ale také
-            krásně vypadají, a to na různé příležitosti, jako jsou narozeniny,
-            svatby, oslavy a další speciální události.
-          </p>
-          <p>
-            Od své babičky jsem se v malém městě [Počátky], kde jsem vyrůstala,
-            naučila vařit a péct. Pečení dortů pro mě znamená vášeň a ráda
-            experimentuji s novými recepty.
-          </p>
-          <p>
-            Pokud hledáte někoho, kdo pro Vás vytvoří ten nejkrásnější a
-            nejchutnější dort na Vaší další speciální událost, neváhejte mě
-            kontaktovat. Těším se na spolupráci s Vámi!
-          </p>
-        </section>
-      </main>
+      <section className={undefined}>
+        <div className={container}>
+          <h1>Stránka o mě</h1>
+          <div>
+            <p>
+              Ahoj, mé jméno je Lenka a pečení dortů je mým koníčkem. Mým cílem
+              je poskytnout zákazníkům dorty, které nejen vynikají chutí, ale
+              také krásně vypadají, a to na různé příležitosti, jako jsou
+              narozeniny, svatby, oslavy a další speciální události.
+            </p>
+            <p>
+              Od své babičky jsem se v malém městě [Počátky], kde jsem
+              vyrůstala, naučila vařit a péct. Pečení dortů pro mě znamená vášeň
+              a ráda experimentuji s novými recepty.
+            </p>
+            <p>
+              Pokud hledáte někoho, kdo pro Vás vytvoří ten nejkrásnější a
+              nejchutnější dort na Vaší další speciální událost, neváhejte mě
+              kontaktovat. Těším se na spolupráci s Vámi!
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
