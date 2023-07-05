ea56707e41705285b6885ba538556077acec7b5f
Jarda Korinek <korinek14.j@gmail.com>
2023-07-05 22:58:13 +0200

    building contact section

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -3,17 +3,25 @@ import GoogleMap from "../components/GoogleMap";
 import Layout from "../components/Layout";
 
 import * as global from "../assets/css/global.module.scss";
+import * as styles from "../pages/kontakty.module.scss";
 
 export default function Kontakt() {
   const { container } = global;
+  const { kontaktHeroImg, headline } = styles;
 
   return (
     <Layout>
       <section className={undefined}>
+        <section id={kontaktHeroImg}>
+          <div className={`${container}`}>
+            <h1 id={headline}>Kontakt</h1>
+          </div>
+        </section>
         <div className={container}>
-          <section>hero image section</section>
-          <section>kontaktni informace section</section>
-          <section>zpusoby kontaktovani section</section>
+          <section>
+            <h2>Kontaktujte mě</h2>
+          </section>
+          <section>ZDE IMPLEMENTOVAT NĚJAKÝ KONTAKTNÍ FORMULÁŘ</section>
           <GoogleMap />
         </div>
       </section>
