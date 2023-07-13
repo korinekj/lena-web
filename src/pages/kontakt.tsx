43efb423e7c6c9393bdf639b9756594970bf7aa1
Jarda Korinek <korinek14.j@gmail.com>
2023-07-13 23:16:37 +0200

    working on images in contact page..a href dynamically is not needed if i have only 6 concrete images i want to display..so i will do it manually and import..then i can use that import value in href and browser will corectly navigate to that image in lightbox..

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -4,13 +4,29 @@ import Layout from "../components/Layout";
 
 import * as global from "../assets/css/global.module.scss";
 import * as styles from "../pages/kontakt.module.scss";
+import { Link } from "gatsby";
+
+import test from "../assets/images/dorty/image00001.jpeg";
+
+//import Test from "../components/Test";
 
 export default function Kontakt() {
   const { container } = global;
-  const { kontaktHeroImg, headline, contactForm, formRow } = styles;
+  const { kontaktHeroImg, headline, contactForm, formRow, wrapper, cake } =
+    styles;
+
+  const images = [
+    "image00062.jpeg",
+    "image00065.jpeg",
+    "image00053.jpeg",
+    "image00059.jpeg",
+    "image00055.jpeg",
+    "image00047.jpeg",
+  ];
 
   return (
     <Layout>
+      {/* <Test /> */}
       <section className={undefined}>
         <section id={kontaktHeroImg}>
           <div className={`${container}`}>
@@ -18,32 +34,42 @@ export default function Kontakt() {
           </div>
         </section>
         <div className={container}>
-          <section id={contactForm}>
-            <h2>Kontaktujte mě</h2>
-            <form>
-              <div className={formRow}>
-                <label htmlFor='fname'>Jméno</label>
-                <input type='text' id='fname' required />
-              </div>
+          <h2>Kontaktujte mě</h2>
 
-              <div className={formRow}>
-                <label htmlFor='lname'>Příjmení</label>
-                <input type='text' id='lname' required />
-              </div>
-
-              <div className={formRow}>
-                <label htmlFor='email'>Email</label>
-                <input type='email' id='email' required />
-              </div>
-
-              <div className={formRow}>
-                <label htmlFor='message'>Vaše zpráva</label>
-                <textarea id='message' rows={10} required></textarea>
-              </div>
-
-              <button type='submit'>Odeslat</button>
-            </form>
-          </section>
+          <div className={wrapper}>
+            <section id={contactForm}>
+              <form>
+                <div className={formRow}>
+                  <label htmlFor='fname'>Jméno</label>
+                  <input type='text' id='fname' required />
+                </div>
+                <div className={formRow}>
+                  <label htmlFor='lname'>Příjmení</label>
+                  <input type='text' id='lname' required />
+                </div>
+                <div className={formRow}>
+                  <label htmlFor='email'>Email</label>
+                  <input type='email' id='email' required />
+                </div>
+                <div className={formRow}>
+                  <label htmlFor='message'>Vaše zpráva</label>
+                  <textarea id='message' rows={10} required></textarea>
+                </div>
+                <button type='submit'>Odeslat</button>
+              </form>
+            </section>
+            <section id={cake}>
+              {images.map((image, index) => (
+                <a href={test} data-lightbox='dort' data-title={image}>
+                  <img
+                    src={require(`../assets/images/dorty/${image}`).default}
+                    alt={`Image ${index + 1}`}
+                    key={index}
+                  />
+                </a>
+              ))}
+            </section>
+          </div>
           <GoogleMap />
         </div>
       </section>
