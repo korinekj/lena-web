30e8266c77c6cc69653607faddc8b4ba4859d215
Jarda Korinek <korinek14.j@gmail.com>
2023-08-24 00:05:40 +0200

    Resized and compressed dorty images

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `dorty`,
-        path: `${__dirname}/src/assets/images/dorty`,
+        path: `${__dirname}/src/assets/images/dorty/resized/compressed`,
       },
     },
     {
